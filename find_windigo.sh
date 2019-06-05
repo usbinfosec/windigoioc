347925ea8ebabfe413e6d5b848f7ed35379f8522
Information Security Tid Bits <51415048+usbinfosec@users.noreply.github.com>
2019-06-05 12:41:29 -0400

    Initial commit
    Initial Commit

---
+++ find_windigo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+library=libkeyutils.so.1
+LIBLOC=$(whereis ${library} | cut -d":" -f2 | tr -d " ")
+
+if [ ! "${LIBLOC}" = "" ]; then
+  output=$(objdump -x "${LIBLOC}" | grep NEEDED)
+  
+  lines=$(wc -l <<< "${output}")
+  
+  if [ ${lines} -gt 2 ]; then
+    printf "You probably have a problem, more than one NEEDED symbol is present\n> %s\n" "${output}"
+  else
+    printf "You seem to be ok\n"
+  fi
+else
+  printf "Can't locate ${library}, sorry...\n"
+fi
